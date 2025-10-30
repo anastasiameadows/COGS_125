f820fe344a95014730d64f02ab6dfb1fdaa19084
Anastasia Meadows <anastasiameadows@Mac.lan>
2025-10-29 22:15:29 -0700

    Updates

---
+++ _next/static/ESxU45HLV01NHkpCDXLwe/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/COGS_125//_next/:path+",
+        "destination": "/COGS_125/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
