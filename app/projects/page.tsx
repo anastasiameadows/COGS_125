9d22e81177cfb1aef1530e1bcc0d224f9644c2b3
Anastasia Meadows <anastasiameadows@Anastasias-MacBook-Air-2.local>
2025-10-30 11:30:03 -0700

    added page.tsx to projects

---
+++ app/projects/page.tsx
@@ -0,0 +1,13 @@
+const ProjectsLandingPage = () => {
+  return (
+    
+    <main>
+      <h1>Select a Project - fixed</h1>
+      {/* This is a temporary page. You can add links here later,
+        or simply have it render nothing.
+      */}
+    </main>
+  );
+};
+
+export default ProjectsLandingPage;
